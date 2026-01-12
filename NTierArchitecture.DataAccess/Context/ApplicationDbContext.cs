0b80c858dcc98833ce281b0144cf05438200e73b
omeruren <omer.uren19@gmail.com>
2026-01-12 11:53:00 +0300

    Created Data Access Layer
    created Context

---
+++ NTierArchitecture.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NTierArchitecture.DataAccess.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
