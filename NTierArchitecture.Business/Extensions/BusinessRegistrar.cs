3237db8a1a7c8b9e334c9e203fbef6a7afa1a356
omeruren <omer.uren19@gmail.com>
2026-01-12 14:01:46 +0300

    Business Service Extension

---
+++ NTierArchitecture.Business/Extensions/BusinessRegistrar.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NTierArchitecture.Business.Categories;
+using NTierArchitecture.Business.Orders;
+using NTierArchitecture.Business.Products;
+
+namespace NTierArchitecture.Business.Extensions;
+
+public static class BusinessRegistrar
+{
+    public static IServiceCollection AddBusiness(this IServiceCollection services)
+    {
+        // Register business services here
+
+        services.AddTransient<CategoryService>();
+        services.AddTransient<ProductService>();
+        services.AddTransient<OrderService>();
+
+        return services;
+    }
+}
