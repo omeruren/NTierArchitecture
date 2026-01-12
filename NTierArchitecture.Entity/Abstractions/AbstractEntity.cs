47090adf5fdfebdde2f950a00a116ebc7a4ea1da
omeruren <omer.uren19@gmail.com>
2026-01-12 11:42:50 +0300

    created Abstract entity

---
+++ NTierArchitecture.Entity/Abstractions/AbstractEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace NTierArchitecture.Entity.Abstractions;
+
+public abstract class AbstractEntity
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+}
