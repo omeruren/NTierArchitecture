a2db8c88d03093eba3e921ad9a85a801321b61f0
omeruren <omer.uren19@gmail.com>
2026-01-12 16:52:42 +0300

    installed openApi and Scalar

--- NTierArchitecture.WebAPI/Program.cs
+++ NTierArchitecture.WebAPI/Program.cs
@@ -2,13 +2,18 @@
 using Carter;
 using NTierArchitecture.Business.Extensions;
 using NTierArchitecture.DataAccess.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataAccess(builder.Configuration).AddBusiness();
 builder.Services.AddCarter();
+builder.Services.AddOpenApi();
+
+
 var app = builder.Build();
 
 app.MapCarter();
-
+app.MapOpenApi();
+app.MapScalarApiReference();
 app.Run();
